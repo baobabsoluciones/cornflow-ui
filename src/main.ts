8c547115d33933dba636376f2b25d3dafc09de82
Javier Garcia <javier.garcia@baobabsoluciones.es>
2024-05-10 12:42:53 +0200

    MInputField migration DONE

--- src/main.ts
+++ src/main.ts
@@ -20,17 +20,19 @@ import './app/assets/styles/main.css' // Main app custom styles
 import './app/assets/styles/variables.css' // App custom variables
 import '@/plugins'
 import 'mango-ui/dist/style.css'
+
 import { MAppDrawer } from 'mango-ui'
 import { MFilterSearch } from 'mango-ui'
 import { MButton } from 'mango-ui'
 import { MFormSteps } from 'mango-ui'
 import { MDragNDropFile } from 'mango-ui'
 import { MSnackbar } from 'mango-ui'
-
 import { MTitleView } from 'mango-ui'
 import { MAppBarTab } from 'mango-ui'
 import { MInfoCard } from 'mango-ui'
 import { MPanelData } from 'mango-ui'
+import { MInputField } from 'mango-ui'
+
 
 const pinia = createPinia()
 const app = createApp(App)
@@ -47,6 +49,8 @@ app.component('MAppBarTab', MAppBarTab)
 app.component('MInfoCard', MInfoCard)
 app.component('MPanelData', MPanelData)
 app.component('MSnackbar', MSnackbar)
+app.component('MInputField', MInputField)
+
 
 app.use(pinia)
 app.mount('#app')
